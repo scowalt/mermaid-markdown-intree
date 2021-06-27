9c50923512b8d6255b81454730e7edf353452756
Scott Walters <me@scowalt.com>
2021-06-26 17:52:00 -0700

    Test

--- dev_scripts/precommit.sh
+++ dev_scripts/precommit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Query the modified *.mmd files
-git diff --cached --name-only --diff-filter=ACM | grep .mmd | xargs npx mmdc -i
+git diff --cached --name-only --diff-filter=ACM | grep .mmd$ | xargs npx mmdc -i
 git add ./**/**.mmd.svg
